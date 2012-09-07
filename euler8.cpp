9e95b90b67dc388cccad043c64c54a07fc924d79
Jason Blanchard <jasonmblanchard@gmail.com>
2012-09-07 10:36:27 -0400

    Now reads characters from file, stores in int array.

--- euler8.cpp
+++ euler8.cpp
@@ -4,11 +4,31 @@
 
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 int main() {
-    std::ofstream file;
+    std::ifstream file;
+    char c = ' ';
+    int array_index = 0;
+    int array[5] = {0,0,0,0,0};
 
     file.open("number", std::ios::in);
+
+    if (file.is_open()) {
+        while (file.good()) {
+            c = file.get();
+
+            if (c >= '0' && c <= '9') {
+                array[array_index] = atoi(&c);
+            }
+
+            ++array_index;
+            if (array_index > 4)
+                array_index = 0;
+        }
+    } else {
+        std::cout << "Error opening file." << std::endl;
+    }
 
     file.close();
 
